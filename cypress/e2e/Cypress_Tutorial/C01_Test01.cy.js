077bcdb05f5a6a22b18ad3cf79a67a62911339f7
YavuzSelimHasturk <yavuzselimhasturk@gmail.com>
2023-08-01 15:51:29 +0300

    first commit

---
+++ cypress/e2e/Cypress_Tutorial/C01_Test01.cy.js
@@ -0,0 +1,25 @@
+
+describe('Temel Cypress Komutlari', () => {
+    it('cy.visit komutu', () => {
+        cy.visit('https://www.amazon.com') // base url tanimli degilse
+        cy.visit('/') // base url tanimli ise
+        
+    });
+
+    it('cy.visit komutu.2', () => {
+      //  cy.visit('/en/faqs')  // url in alt sayfasina erisim  
+    });
+
+    it('cy.visit title komutu', () => {
+        cy.visit('/')
+        cy.title().should('equal','Amazon.com. Spend less. Smile more.')  // tam esitlik durumu
+        cy.title().should('include','Amazon') // verilen string icerme durumu
+        cy.title().should('contains', 'Spend less')
+    });
+    it('cy.url komutu', () => {
+        cy.visit('/')
+        cy.url().should('eq','https://www.amazon.com/') // tam esitlik durumu
+        cy.url().should('include','mazo')
+
+    });
+});
