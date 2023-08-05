b42583472b03ad01f166541c5660e24465baac36
YavuzSelimHasturk <yavuzselimhasturk@gmail.com>
2023-08-05 20:54:36 +0300

    05.08.2023

--- cypress/e2e/Cypress_Tutorial/day04/C04_Scroll.cy.js
+++ cypress/e2e/Cypress_Tutorial/day04/C04_Scroll.cy.js
@@ -4,7 +4,7 @@ describe('Scroll Islemleri', () => {
     it('Scroll ile sayfayi belirli bir yere kaydirmak', () => {
         cy.visit('https://tripandway.com')
         cy.get('.portfolio-page > .container > :nth-child(1) > .col-md-12 > .main-headline > .headline > h2').scrollIntoView({duration:1000})
-        // Locate alinan yere kaydirir duration ile belirttigimiz sure ne kadar zamanda oraya ilerleyecegi suresi
+        // Locate alinan yere kaydirir duration ile belirttigimiz sure ne kadar zamanda oraya ilerleyecegi suresi.
         cy.wait(3000)
         cy.get('.service-area > .container > :nth-child(1) > .col-md-12 > .main-headline > .headline > h2').scrollIntoView()
 
